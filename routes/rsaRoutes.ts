37196f42c7709454284d3530d412f5ae32c5abe5
Sergio Gras <sergiogras99@gmail.com>
2022-06-08 20:32:32 +0200

    added to routes

--- routes/rsaRoutes.ts
+++ routes/rsaRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {getPublicKeyRSA, createRsaPair, encryptMessage, decrypt, signMessage, verifyMessage, verifyBlind, getBlind} from '../controller/rsaController'
+import {getPublicKeyRSA, createRsaPair, encryptMessage, decrypt, signMessage, verifyMessage, verifyBlind, getBlind, unblind} from '../controller/rsaController'
 
 
 const router = Router();
@@ -12,7 +12,8 @@ router.post('/verify', verifyMessage);
 router.post('/verifyBlind', verifyBlind);
 router.post('/sign', signMessage)
 //router.post('/signBlind', signBlind)
-router.get('/hash', getBlind)
+router.get('/blind', getBlind)
+router.post('/unblind', unblind)
 
 
 export default router;
