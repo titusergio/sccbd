8bf080f6609eb08f1cad7fb945c10a85ec47a70b
Sergio Gras <sergiogras99@gmail.com>
2022-06-08 21:12:44 +0200

    message blinded

--- routes/rsaRoutes.ts
+++ routes/rsaRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {getPublicKeyRSA, createRsaPair, encryptMessage, decrypt, signMessage, verifyMessage, verifyBlind, getBlind, unblind} from '../controller/rsaController'
+import {getPublicKeyRSA, createRsaPair, encryptMessage, decrypt, signMessage, verifyMessage, verifyBlind, getBlind, unblind, signBlind} from '../controller/rsaController'
 
 
 const router = Router();
@@ -11,7 +11,7 @@ router.post('/decrypt', decrypt)
 router.post('/verify', verifyMessage);
 router.post('/verifyBlind', verifyBlind);
 router.post('/sign', signMessage)
-//router.post('/signBlind', signBlind)
+router.post('/signBlind', signBlind)
 router.get('/blind', getBlind)
 router.post('/unblind', unblind)
 
